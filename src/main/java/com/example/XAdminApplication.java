07a293c448c7d65e13dee84b5e4dfa7d705b313b
zstupi <1348524083@qq.com>
2024-05-13 18:26:51 +0800

    模板初始化

---
+++ src/main/java/com/example/XAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XAdminApplication.class, args);
+    }
+
+}
